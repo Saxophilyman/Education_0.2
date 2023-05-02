d5e7b027ca85b473638de287d9ea042cba9af5da
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-05-02 21:43:52 +0400

    Update 0004_Stack.java

--- 0004_Stack.java
+++ 0004_Stack.java
@@ -1,1 +1,39 @@
+import java.util.*;
 
+public class Stack<T> {
+    private T pop;
+    private int count;
+    private ArrayList<T> arrList;
+
+    public Stack() {
+        // инициализация внутреннего хранилища стека
+        arrList = new ArrayList<>();
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public T pop() {
+        if (count == 0) {
+            return null;  // если стек пустой
+        }
+        count--;
+        pop = arrList.get(count);
+        arrList.remove(count);
+
+        return pop;
+    }
+
+    public void push(T val) {
+        arrList.add(val);
+        count++;
+    }
+
+    public T peek() {
+        if (count == 0) {
+            return null;  // если стек пустой
+        }
+        return arrList.get(count - 1);
+    }
+}
