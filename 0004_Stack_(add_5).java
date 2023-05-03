f733ab4f1cd364907b4f895670cb66d689be37eb
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-05-03 11:59:54 +0400

    Create 0004_Stack_(add_5).java

---
+++ 0004_Stack_(add_5).java
@@ -0,0 +1,31 @@
+    public static int postfix(String s) {
+        String[] symbols = s.split(" ");
+        Stack<String> stack = new Stack<String>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+        for (int i = symbols.length - 1; i >= 0; i--) {
+            stack.push(symbols[i]);
+        }
+        while (stack.size() > 0) {
+            int x = Integer.MIN_VALUE;
+            try {
+                x = Integer.parseInt(stack.peek());
+            } catch (NumberFormatException e) {
+
+            }
+            if (x != Integer.MIN_VALUE) {
+                stack2.push(Integer.valueOf(stack.pop()));
+            } else if (stack.peek().equals("+")) {
+                stack.pop();
+                x = stack2.pop() + stack2.pop();
+                stack2.push(x);
+            } else if (stack.peek().equals("*")) {
+                stack.pop();
+                x = stack2.pop() * stack2.pop();
+                stack2.push(x);
+            } else if (stack.peek().equals("=")) {
+                stack.pop();
+                return stack2.pop();
+            }
+        }
+        return Integer.parseInt(null);
+    }
