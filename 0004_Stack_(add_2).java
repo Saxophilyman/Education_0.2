a92baa8010c450a0a7e5eafebeb5f26384aa6499
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-05-03 11:57:12 +0400

    Create 0004_Stack_(add_2).java

---
+++ 0004_Stack_(add_2).java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Stack<T> {
+    private LinkedList<T> linkedList;
+
+    public Stack() {
+        linkedList = new LinkedList<>();
+    }
+
+    public int size() {
+        return linkedList.size();
+    }
+
+    public T pop() {
+        return linkedList.pollFirst();
+    }
+
+    public void push(T val) {
+        linkedList.addFirst(val);
+    }
+
+    public T peek() {
+        return linkedList.peekFirst(); // если стек пустой
+    }
+}
